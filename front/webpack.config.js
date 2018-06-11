6060e74204387e2d97061132c4676fbe00e6f2b1
lacaux-gerome <gerome.lacaux@hetic.net>
2018-06-11 16:40:14 +0200

    init project front await to add twig

---
+++ front/webpack.config.js
@@ -0,0 +1,61 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var webpack = require('webpack')
+var path = require('path')
+
+var isProd = process.env.NODE_ENV === 'production';
+var cssDev = ['style-loader', 'css-loader', 'sass-loader']
+var cssProd = ExtractTextPlugin.extract({
+  fallback: 'style-loader',
+  use: ['css-loader', 'sass-loader']
+});
+var cssConfig = isProd ? cssProd : cssDev;
+
+module.exports = {
+  entry: {
+    'index': './src/js/index.js'
+  },
+  devtool: isProd ? '' : 'eval-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: cssConfig
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    hot: true,
+    open: false
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin(),
+    new ExtractTextPlugin({
+      filename: 'styles.css',
+      disable: !isProd,
+      allChunks: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new UglifyJSPlugin()
+  ]
+}
