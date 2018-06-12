1f0097885cdd78de48b4f68440ec2a51e22772c4
AmFlint <amasselot@hotmail.com>
2018-06-12 17:00:05 +0200

    [Controller/Post/View] Create PostController::ViewAction for post view

--- core/controllers/PostController.php
+++ core/controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Controllers;
 use App\Entities\Post;
 use App\Helpers\Entity;
-use App\Models\PostModel;
+use App\Repositories\PostRepository;
 use Symfony\Component\HttpFoundation\Request;
 
 class  PostController extends BaseController
@@ -24,16 +24,15 @@ class  PostController extends BaseController
      */
     public function saveAction()
     {
-        $postModel = new PostModel();
+        $postRepository = new PostRepository();
         $post = new Post($_POST);
         $validateViolations = $post->validate();
 
         if (count($validateViolations)!== 0) {
             return $validateViolations;
         } else {
-            $postModel->create($post);
+            $postRepository->create($post);
         }
-
     }
 
     /**
@@ -45,9 +44,36 @@ class  PostController extends BaseController
      */
     public function listAction(): string
     {
-        $postModel = new PostModel();
-        $posts = $postModel->getAll();
+        $postRepository = new PostRepository();
+        $posts = $postRepository->getAll();
 
-        return $this->render('posts/list.html.twig', ['posts' => $posts]);
+        // Retrieve errors from Query Parameters and pass it to view
+        $error = isset($_GET['error']) ? $_GET['error'] : '';
+        return $this->render('posts/list.html.twig', [
+            'posts' => $posts,
+            'error' => $error,
+        ]);
+    }
+
+    /**
+     * View a post by id
+     * @param int $id - Id of the post to view
+     * @return string - HTML Structure for page posts/view
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function viewAction($id): string
+    {
+        $postModel = new PostRepository();
+        $post = $postModel->getById($id);
+
+        if (empty($post)) {
+            $error = "Post with id: $id does not exist";
+            header("Location: /posts?error=$error");
+            exit;
+        }
+
+        return $this->render('posts/view.html.twig', ['post' => $post]);
     }
 }
