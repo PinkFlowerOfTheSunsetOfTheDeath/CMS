6d125ca03a1b80aad67cdf58bc8095c678376799
AmFlint <amasselot@hotmail.com>
2018-06-13 11:16:13 +0200

    [Routing+Action/CreateCategory] Create route + handler for create category

---
+++ core/controllers/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controllers;
+use App\Helpers\Controller;
+
+/**
+ * Class CategoryController
+ * @package App\Controllers
+ */
+class CategoryController extends Controller
+{
+    /**
+     * Create a Category via the category creation Form
+     * @return string - HTML Layout for categories creation form
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function createAction(): string
+    {
+        return $this->render('categories/form.html.twig');
+    }
+}
