2547a7c6e6d509d2ca82e5e9c1745622863723ac
AmFlint <amasselot@hotmail.com>
2018-06-13 15:04:27 +0200

    [Categories/Create] Finish CategoryController::save error management + save

--- core/controllers/CategoryController.php
+++ core/controllers/CategoryController.php
@@ -1,6 +1,10 @@
 <?php
 namespace App\Controllers;
+use App\Entities\Category;
 use App\Helpers\Controller;
+use App\Helpers\ErrorManager;
+use App\Repositories\CategoryRepository;
+use Twig\Error\Error;
 
 /**
  * Class CategoryController
@@ -20,8 +24,31 @@ class CategoryController extends Controller
         return $this->render('categories/form.html.twig');
     }
 
+    /**
+     *
+     * @return string
+     */
     public function saveAction(): string
     {
+        $category = new Category($_POST);
+        $violations = $category->validate();
 
+        if (!empty($violations)) {
+            ErrorManager::setError($violations);
+            $_SESSION['form'] = $_POST;
+            $this->redirect('/categories/create');
+            exit;
+        }
+        $categoryRepository = new CategoryRepository();
+        // try to save category to database, if succeed, redirect to details page, else redirect to creation form
+        try {
+            $category = $categoryRepository->create($category);
+            $this->redirect("/categories/$category->id");
+            exit;
+        } catch (\PDOException $e) {
+            ErrorManager::setError([$e->getMessage()]);
+            $this->redirect('/categories/create');
+            exit;
+        }
     }
 }
