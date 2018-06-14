00640e22563275ecc84e91a1b54f563011091347
AmFlint <amasselot@hotmail.com>
2018-06-14 10:56:52 +0200

    [Clean/index] Clean Index.php -> Use kernel boot

--- public/index.php
+++ public/index.php
@@ -4,59 +4,7 @@ require '../vendor/autoload.php';
 
 session_start();
 
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Routing\Loader\YamlFileLoader;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\HttpFoundation\Request;
-use App\Helpers\Controller;
-
-
-
-// Request Object from Http foundation
-$request = Request::createFromGlobals();
-
-// Loader configuration file for routing
-$fileLocator = new FileLocator([__DIR__ . '/../core/config']);
-$loader = new YamlFileLoader($fileLocator);
-$routes = $loader->load('routing.yaml');
-
-
-// Create request context for routing
-$requestContext = new RequestContext();
-$requestContext->fromRequest($request);
-
-
-$matcher = new UrlMatcher($routes, $requestContext);
-
-try {
-    $parameters = $matcher->match($request->getPathInfo());
-} catch (Symfony\Component\Routing\Exception\ResourceNotFoundException $e) {
-    // Display 404.php
-    http_response_code(404);
-    echo Controller::error('You are trying to access a route which does not exist');
-    exit;
-}
-
-$controllerName = explode('::', $parameters['_controller'])[0];
-$actionName = explode('::', $parameters['_controller'])[1];
-
-// TODO: Check that Controller and method exists before using it
-try {
-    $controller = new $controllerName();
-} catch (Exception $e) {
-    echo Controller::error($e->getMessage());
-    exit;
-}
-
-// Unset controller and route parameters
-unset($parameters['_controller']);
-unset($parameters['_route']);
-
-// Execute
-try {
-    echo call_user_func_array([$controller, $actionName], $parameters);
-} catch (Exception $e) {
-   echo Controller::error($e->getMessage());
-}
-
+// Boot Application, Initialize configuration and run
+$kernel = new \App\Boot\Kernel();
+$kernel->bootApplication();
+$kernel->run();
