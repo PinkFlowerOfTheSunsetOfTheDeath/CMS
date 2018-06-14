76f7e00576fe13d89117269808c74a72d0aa5926
mariecharles <marie.charles@hetic.net>
2018-06-14 11:49:36 +0200

    [core/controllers] fix SQL request to list pages

--- core/controllers/PageController.php
+++ core/controllers/PageController.php
@@ -19,6 +19,8 @@ class PageController extends Controller
 
         $error = isset($_GET['error']) ? $_GET['error'] : '';
 
+        dump($pages);
+        die();
         return $this->render("pages/listPages.html.twig", [
             'pages' => $pages,
             'error' => $error
--- core/repositories/PageRepository.php
+++ core/repositories/PageRepository.php
@@ -16,37 +16,35 @@ class PageRepository extends Repository
 {
     public function getAll($slug = '', $id = 0)
     {
-        $sql = 'SELECT `id`, `title`, `content`, `created_at`, `updated_at`, `visibility`, `slug` FROM `pages`';
+        $sql = 'SELECT `id`, `title`, `content`, `slug`, `content`, `visibility` FROM `pages`';
 
         $sqlCond = [];
 
         if(!empty($slug)) {
-            $sqlCond = '`slug` = :slug';
+            $sqlCond[] = '`slug` = :slug';
         }
 
         if(!empty($id)) {
-            $sqlCond = '`id` = :id';
+            $sqlCond[] = '`id` = :id';
         }
 
         if (count($sqlCond) > 0) {
-            $sql.= " WHERE " . implode(" AND " , $sqlCond);
+            $sql.= ' WHERE ' . implode(' AND' , $sqlCond);
         }
 
         $stmt = $this->getDB()->prepare($sql);
 
-        if(!empty($slug)) {
+        if (!empty($slug)) {
             $stmt->bindValue('slug', $slug);
         }
 
-        if(!empty($id)) {
+        if (!empty($id)) {
             $stmt->bindValue('slug', $id);
         }
 
         $stmt->execute();
         $rows = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-
         $pages = [];
-
         foreach ($rows as $row) {
             $pages[] = new Page($row);
         }
