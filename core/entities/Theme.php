a967da14a30438c914ee3d505cecbd9e1734b210
AmFlint <amasselot@hotmail.com>
2018-06-14 14:42:56 +0200

    [Entity/Theme] Create theme entity

---
+++ core/entities/Theme.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Entities;
+use App\Helpers\Entity;
+
+/**
+ * Class Theme
+ * @package App\Entities
+ */
+class Theme extends Entity
+{
+    public $image = '';
+    public $name = '';
+}
